400925a8002fcb7cd9ec830ea21ba8aa0ec8b372
John <johnmdrake@gmail.com>
2019-04-30 16:26:51 -0500

    Update ctrlsHouses.js to sort on number field.

--- ctrls/ctrlsHouses.js
+++ ctrls/ctrlsHouses.js
@@ -1,17 +1,27 @@
 var dbHouses = new PouchDB("https://thathersessallyredernsin:5b391b98ab31e3c53b65c6aaa872698ec8bbdb39@55644244-4beb-4ca8-b177-8ff6d5c3cc0b-bluemix.cloudant.com/houselist"); 
 
+dbHouse.createIndex({
+  index: {fields: ['number']}
+});
+
+
 function ctrlsHousesGetHouseList(callback){
     var results = [];
-    dbHouses.allDocs({include_docs:true}).then(function(res){
+	dbHouses.find({
+	  selector: {
+		name: {$gte: null}
+	  },
+	  sort: ['name']
+	}).then(function(res){
         if(res) {
             for(var i=0; i<res.rows.length; i++) {
                 results.push(res.rows[i].doc);
             }
         }
         callback(results);
-    }).catch(function(err){
-        callback(err)
-    });
+	}).catch(function(err){
+		callback(err)
+	});
 }
 
 function ctrlsHousesAddHouse(number, address, image, callback) {
